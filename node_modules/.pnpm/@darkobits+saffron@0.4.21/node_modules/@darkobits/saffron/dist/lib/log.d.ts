c4ed74138b18d126d1c37ddf338ab2642afbd97a
qilin88 <1019921999@qq.com>
2022-05-10 11:39:38 +0800

    aa

---
+++ node_modules/.pnpm/@darkobits+saffron@0.4.21/node_modules/@darkobits/saffron/dist/lib/log.d.ts
@@ -0,0 +1,2 @@
+declare const _default: import("@darkobits/log/dist/etc/types").Logger;
+export default _default;
---
+++ packages/demo/src/index.ts
@@ -0,0 +1,4 @@
+'use strict';
+
+import {demo} from "qilin88/core";
+console.log(demo);
